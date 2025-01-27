621cc5bf19457b40044d34925349941e9d416e1f
Ray Lopez <raylopez509@gmail.com>
2025-01-27 09:45:27 -0800

    added coloring to header and changed title

--- src/index.js
+++ src/index.js
@@ -45,9 +45,60 @@ function displayHomePage() {
 function displayMenuPage() {
   removeContent();
   const contentDiv = document.querySelector('#content');
-  const testDiv = document.createElement('div');
-  testDiv.textContent = '[enter menu page here]';
-  contentDiv.appendChild(testDiv);
+  const menuTitle = document.createElement('h1');
+  menuTitle.textContent = 'Menu';
+  contentDiv.appendChild(menuTitle);
+
+  const appetizersTitle = document.createElement('h1');
+  appetizersTitle.textContent = 'Appetizers';
+  contentDiv.appendChild(appetizersTitle);
+  const appetizersList = document.createElement('ul');
+  const appetizersArr = [
+    'Pajeon',
+    'French Toast',
+    'Guacamole and Chips',
+    'Carne Asada Fries',
+  ];
+  for (let item of appetizersArr) {
+    let menuItem = document.createElement('li');
+    menuItem.textContent = item;
+    appetizersList.appendChild(menuItem);
+  }
+  contentDiv.appendChild(appetizersList);
+
+  const entreesTitle = document.createElement('h1');
+  entreesTitle.textContent = 'Entrees';
+  contentDiv.appendChild(entreesTitle);
+  const entreesList = document.createElement('ul');
+  const entreesArr = [
+    'Sushi',
+    'Korean BBQ',
+    'Quesabirria Tacos',
+    'Orange Chicken',
+  ];
+  for (let item of entreesArr) {
+    let menuItem = document.createElement('li');
+    menuItem.textContent = item;
+    entreesList.appendChild(menuItem);
+  }
+  contentDiv.appendChild(entreesList);
+
+  const dessertsTitle = document.createElement('h1');
+  dessertsTitle.textContent = 'Desserts';
+  contentDiv.appendChild(dessertsTitle);
+  const dessertsList = document.createElement('ul');
+  const dessertsArr = [
+    'Madellines',
+    'Banana Pudding',
+    'Ice Cream',
+    'Cheesecakes',
+  ];
+  for (let item of dessertsArr) {
+    let menuItem = document.createElement('li');
+    menuItem.textContent = item;
+    dessertsList.appendChild(menuItem);
+  }
+  contentDiv.appendChild(dessertsList);
 }
 
 function displayAboutPage() {
