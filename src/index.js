b7190af853779bfaac6711f0e0b96233eec43691
Ray Lopez <raylopez509@gmail.com>
2025-01-26 16:09:45 -0800

    added webpack for css, added fonts, kept html kept html for when user initially gets into site

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+import './styles.css';
+
 function displayHomePage() {
   removeContent();
   const contentDiv = document.querySelector('#content');
-  const title = document.createElement('div');
-  title.textContent = 'Tabletop Tavern';
-  contentDiv.appendChild(title);
   const description = document.createElement('p');
   description.textContent =
     "We got food and we got board games! Please don't touch our games with your greasy fingers";
   contentDiv.appendChild(description);
-  const locationHeader = document.createElement('div');
+  const locationHeader = document.createElement('h1');
   locationHeader.textContent = 'Location';
   contentDiv.appendChild(locationHeader);
   const address = document.createElement('p');
@@ -19,7 +18,6 @@ function displayHomePage() {
   contentDiv.appendChild(hoursHeader);
 
   const hoursList = document.createElement('ul');
-
   const hoursArr = [
     'Sunday: 11am-11pm',
     'Monday: 12pm-11pm',
@@ -34,27 +32,6 @@ function displayHomePage() {
     time.textContent = day;
     hoursList.appendChild(time);
   }
-  // const sunday = document.createElement("li");
-  // sunday.textContent = "Sunday: 11am-11pm";
-  // const monday = document.createElement("li");
-  // monday.textContent = "Monday: 12pm-11pm";
-  // const tuesday = document.createElement("li");
-  // tuesday.textContent = "Tuesday: 12pm-11pm";
-  // const wednesday = document.createElement("li");
-  // wednesday.textContent = "Wednesday: Closed";
-  // const thursday = document.createElement("li");
-  // thursday.textContent = "Thursday: 12pm-11pm";
-  // const friday = document.createElement("li");
-  // friday.textContent = "Friday: 12pm-1am";
-  // const saturday = document.createElement("li");
-  // saturday.textContent = "Saturday: 12pm-1am";
-  // hoursList.appendChild(sunday);
-  // hoursList.appendChild(monday);
-  // hoursList.appendChild(tuesday);
-  // hoursList.appendChild(wednesday);
-  // hoursList.appendChild(thursday);
-  // hoursList.appendChild(friday);
-  // hoursList.appendChild(saturday);
   contentDiv.appendChild(hoursList);
 
   const phoneHeader = document.createElement('h1');
